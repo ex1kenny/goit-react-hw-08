4c43faa91e504a1fd9016abe4357bbca483e454e
ex1kenny <ex1kenny4543@gmail.com>
2024-03-29 19:48:31 +0200

    home-work8.3

--- src/components/AuthNavigation/AuthNavigation.jsx
+++ src/components/AuthNavigation/AuthNavigation.jsx
@@ -6,15 +6,17 @@ import { selectIsLoggedIn } from "../../redux/auth/slice";
 export default function AuthNavigation() {
   const isLoggedIn = useSelector(selectIsLoggedIn);
   return (
-    <nav className={css.wrapper}>
-      <NavLink to="/" className={css.link}>
-        Home
-      </NavLink>
-      {isLoggedIn && (
-        <NavLink to="/contacts" className={css.link}>
-          Contacts
+    <div className={css.con}>
+      <nav className={css.wrapper}>
+        <NavLink to="/" className={css.link}>
+          Home
         </NavLink>
-      )}
-    </nav>
+        {isLoggedIn && (
+          <NavLink to="/contacts" className={css.link}>
+            Contacts
+          </NavLink>
+        )}
+      </nav>
+    </div>
   );
 }
